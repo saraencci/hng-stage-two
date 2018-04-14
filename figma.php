e92aef2d735ea5b68d6f9b8385f4dd55a5e364d5
saraencci <franksaraencci@gmail.com>
2018-04-14 21:29:29 +0530

    GGgG

---
+++ figma.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<html>
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Righteous">
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Overpass">
+
+<style type="text/css">
+
+#mid
+{
+ 
+   color: #FBF7F7;
+   width: 67%;
+   border-radius: 30px;
+   padding-top: 30px;
+   font-size: 38px;
+   padding-bottom: 40px;
+   font-family: 'Font Name',Overpass;
+   background-color:rgba(238, 29, 29, 0.34);
+}
+body
+{
+	width: 100%;
+	color: #000000;
+	padding-top: 100px;
+	font-family: 'Font Name',Righteous;
+	text-align: center;
+	font-size: 48px;
+	font-style:regular;
+	line-height: 60px;
+	
+
+   background-color:rgba(196, 196, 196, 0.50);
+}
+
+</style>
+<head>
+	<title>
+		FIGMA
+
+	</title>
+</head>
+<body>
+
+	 HNG INTERNSHIP 4.0<br>
+	 <c style="color: #FBF7F7;">WELCOME TO OUR 4TH INTERNSHIP</c><br><br>
+	<center><div id="mid">
+		 <br>
+		 
+		<?php
+echo date("F d, Y ") . "<br>Watch the clock beat deadlines<br> ";
+echo "It is now ".date("h:i a");
+?> 
+		
+
+	</div></center>
+
+</body>
+</html>
